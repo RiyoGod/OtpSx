746f3c4bc8628d2c7e364b026a7b5ffa90a7c788
RiyoGod <godsxrio@gmail.com>
2025-03-19 15:52:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = 26416419  # Replace with your Telegram API ID
+API_HASH = "c109c77f5823c847b1aeb7fbd4990cc4"  # Replace with your Telegram API Hash
+BOT_TOKEN = "7955234990:AAH91z9G-Ytz3xGhil7Ermy0l9NvOJufEFQ"  # Replace with your Bot Token from @BotFather
+OWNER_ID = 6748827895  # Replace with your Telegram User ID (Owner)
